c341d5f2141ab6901e564d20bcf0a323bae34348
Illia <daliakillia@gmail.com>
2019-05-01 16:51:51 +0300

    HW_4''Python functionaly''

--- build_in_methods_iterators/homework.py
+++ build_in_methods_iterators/homework.py
@@ -1,4 +1,7 @@
 from typing import List, Dict, Union, Generator
+import string
+import random
+import functools
 
 # We will work with such dicts
 ST = Dict[str, Union[str, int]]
@@ -14,7 +17,11 @@ def task_1_fix_names_start_letter(data: DT) -> DT:
         fix_names_start_letters([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}])
         >>> [{'name': 'Alex', 'age': 26}, {'name': 'Denys', 'age': 89}]
     """
-    pass
+    for member in data:
+        if member.get('name') is not None:
+            member['name'] = member['name'].capitalize()
+    return data
+
 
 
 def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
@@ -24,8 +31,9 @@ def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
     Examples:
        remove_dict_field([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 'age')
         >>> [{'name': 'Alex'}, {'name': 'denys'}]
+
     """
-    pass
+    return [{k: v for k, v in i.items() if k not in redundant_keys} for i in data]
 
 
 def task_3_find_item_via_value(data: DT, value) -> DT:
@@ -35,21 +43,25 @@ def task_3_find_item_via_value(data: DT, value) -> DT:
         find_item_via_value([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 26)
         >>> [{'name': 'Alex', 'age': 26}]
     """
-    pass
+    return [d for d in data if value in d.values()]
+
 
 
 def task_4_min_value_integers(data: List[int]) -> int:
     """
     Find and return minimum value from list
     """
-    pass
+    if len(data) > 0:
+        return min(data)
 
 
 def task_5_min_value_strings(data: List[Union[str, int]]) -> str:
     """
     Find the longest string
     """
-    pass
+    if len(data) > 0:
+
+        return str(min(data, key=lambda x: len(str(x))))
 
 
 def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
@@ -58,21 +70,24 @@ def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
     Returns:
 
     """
-    pass
+    filter = [item for item in data if item.get("age", None)]
 
+    return min(filter, key=lambda x: x.get(key))
 
 def task_7_max_value_list_of_lists(data: List[List[int]]) -> int:
     """
     Find max value from list of lists
     """
-    pass
+    max_list = max(map(lambda x: x, data))
+    return max(map(lambda x: x, max_list))
 
 
 def task_8_sum_of_ints(data: List[int]) -> int:
     """
     Find sum of all items in given list
     """
-    pass
+
+    return sum(map(lambda x: x, data))
 
 
 def task_9_sum_characters_positions(text: str) -> int:
@@ -88,7 +103,10 @@ def task_9_sum_characters_positions(text: str) -> int:
         >>> 532
 
     """
-    pass
+
+
+
+    return sum([ord(x) for x in text])
 
 
 def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
@@ -100,9 +118,14 @@ def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
         next(a)
         >>> 2
         next(a)
-        >>> 3
-    """
-    pass
+        >>> 3"""
+
+    for num in range(2,200):
+     if all(num%i!=0 for i in range(2,num)):
+       yield num
+
+
+
 
 
 def task_11_create_list_of_random_characters() -> List[str]:
@@ -110,4 +133,6 @@ def task_11_create_list_of_random_characters() -> List[str]:
     Create list of 20 elements where each element is random letter from latin alphabet
 
     """
-    pass
+
+
+    return [random.choice(string.ascii_letters.lower()) for _ in range(20)]
